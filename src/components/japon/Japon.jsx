08a13035c06cecf41efb5fba68e41c0bdcee7dbb
Luka Burian <lukaburian@gmail.com>
2023-02-05 14:38:01 +0100

    phase 3 japon

--- src/components/japon/Japon.jsx
+++ src/components/japon/Japon.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
-import Logo from '../common/Logo';
-import '../../assets/css/Step1.css';
+import functions from "./functions";
+import bambou1 from '../../assets/images/bambou1.png';
 import '../../assets/css/style.css';
+import '../../assets/css/Step3.css';
 
-const Blogs = () => {
+const japon = () => {
+  const { validateB1, nav, unclicked, recColor } = functions();
     return (
     <>
-    <div id='container'>
-      <Logo />
-      <h1>Vous êtes en SAGI?</h1>
-      <p>Enigme Hub est un site réalisé par des étudiants de SAGI comme projet HTML 5.</p>
+    <div>
+      <div>
+
+      </div>
+      <div id='imperial'>
+        <div id='parchemin' className='imperial-colomn'>
+          text lolilol
+        </div>
+        <div  className='imperial-colomn'>
+          <div id='bamboo'>
+            <button onClick={() => validateB1('test1', 0)} id="bamboo_01"><img id='test1' value={true} src={bambou1}/></button>
+            <button onClick={() => validateB1('test2', 1)} id="bamboo_02"><img id='test2' value={true} src={bambou1}/></button>
+            <button onClick={() => validateB1('test3', 2)} id="bamboo_03"><img id='test3' value={true} src={bambou1}/></button>
+            <button onClick={() => validateB1('test4', 3)} id="bamboo_04"><img id='test4' value={true} src={bambou1}/></button>
+            <button onClick={() => validateB1('test5', 4)} id="bamboo_05"><img id='test5' value={true} src={bambou1}/></button>
+            <button onClick={() => validateB1('test6', 5)} id="bamboo_06"><img id='test6' value={true} src={bambou1}/></button>
+          </div>
+          <button onMouseOver={unclicked} onMouseLeave={recColor} onClick={nav} className='button' id='button'>Validation</button> 
+        </div>
+      </div>
     </div>
     </>
     )
   };
   
-  export default Blogs;
+  export default japon;
   
---
+++ src/components/japon/functions.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import wording from '../../wording.json';
+
+const functions = () => {
+
+  function validateB1(id, num){
+    var but = document.getElementById(id);
+
+    but.value===true?but.src=require('../../assets/images/bambou1.png'):but.src=require('../../assets/images/bambou2.png')
+    but.value = !but.value
+  }
+
+  const nav = () => {
+    window.location.replace(wording.baseUrl + '/');
+  };
+
+  const unclicked = () => {
+    var b1 = document.getElementById('test1').value
+    var b2 = document.getElementById('test2').value
+    var b3 = document.getElementById('test3').value
+    var b4 = document.getElementById('test4').value
+    var b5 = document.getElementById('test5').value
+    var b6 = document.getElementById('test6').value
+    
+    const button = document.getElementById('button'); 
+    if((b2&&b5&&b6)==true&&(b1&&b3&&b4)!=true){
+      button.style.backgroundColor = 'green';      
+    }
+    else{   
+      button.style.transform == 'translate('+0+'px)'?button.style.transform = 'translate('+250+'px)':button.style.transform = 'translate('+0+'px)';
+    }
+  }
+
+  function recColor(){
+    const button = document.getElementById('button'); 
+    button.style.backgroundColor = '#f4511e'
+  }
+  return {
+    validateB1,
+    nav,
+    unclicked,
+    recColor
+  };
+};
+
+export default functions;
